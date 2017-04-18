aabce5118fdd075bbbe1d94a822b22df2c9eab41
MACRIUM\AOsiname <Andrew.Osiname@macrium.com>
2017-04-18 15:37:02 +0100

    open using a batch file

--- Setup.bat
+++ Setup.bat
@@ -1,6 +1,7 @@
-::An attempt to run on single click
-::@ECHO OFF
-::PowerShell.exe -ExecutionPolicy RemoteSigned -Command "& '%~dpn0.ps1'"
-@Powershell.exe Setup.ps1 -base "F:\Code\Presentation-Layer" -projName "Bestest" -projUrl "the-best.com"
-::Powershell.exe -ExecutionPolicy RemoteSigned -Command "& '%~dpn0.ps1'" -base "D:\all\Code\Mine" -proj "the-best.comss"
+:: Project Directory
+:: Project Folder
+:: Projetc Url
+
+powershell -noexit "& ""F:\IIS-Dev-Environment-Setup\Setup.ps1""" -base "F:\MacriumWebsite" -projName "MacriumWebsite" -projUrl "www-dev.macrium.com"
+
 PAUSE
